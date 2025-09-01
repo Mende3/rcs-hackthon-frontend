46125d944403867001d10d716519adeedc8ccc65
dell@SMALL <devsmalldev@gmail.com>
2025-09-01 18:07:15 +0100

    url original

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000'; // URL base correta
+const API_URL = 'https://rcs-hackthon-django.onrender.com'; // URL base correta
 
 export interface User {
   id: number;
