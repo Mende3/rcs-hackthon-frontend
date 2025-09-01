e03abc7dfe4e3fe45a9f64011742d6c5b30539d2
dell@SMALL <devsmalldev@gmail.com>
2025-09-01 19:13:43 +0100

    url de producao

--- src/services/configAxios.ts
+++ src/services/configAxios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { authService } from "./authService";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://rcs-hackthon-django.onrender.com",
 });
 
 // Intercepta requisições → adiciona token
